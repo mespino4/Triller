2859aca38e6f3fea6e9321e53c2e1578a17c1222
MeL <90160548+melyespinofl96@users.noreply.github.com>
2023-12-22 16:18:16 -0600

    first commit

---
+++ api-aspnet/src/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using api_aspnet.src.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_aspnet.src.Data;
+
+public class DataContext : DbContext {
+	public DataContext(DbContextOptions options) : base(options) {
+	}
+
+	public DbSet<AppUser> Users { get; set; }
+}
---
+++ api-aspnet/src/Entities/AppUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace api_aspnet.src.Entities;
+
+public class AppUser : IdentityUser<int>{
+	public DateOnly DateOfBirth { get; set; }
+	public string DisplayName { get; set; }
+
+	public DateTime Created { get; set; } = DateTime.UtcNow;
+	public DateTime LastActive { get; set; } = DateTime.UtcNow;
+	public string Gender { get; set; }
+	public string About { get; set; }
+	public string Location { get; set; }
+	public string ProfilePic { get; set; }
+	public string BannerPic { get; set; }
+
+	public List<UserPhoto> Media { get; set; } = new();
+
+	public List<Connection> Followers { get; set; }
+	public List<Connection> Following { get; set; }
+	public List<Retrill> Retrills { get; set; }
+	public List<Trill> Trills { get; set; } = new();
+	public List<TrillLike> TrillsLiked { get; set; } = new();
+	public List<TrillReply> Replies { get; set; } = new();
+
+	public List<Bookmark> Bookmarks { get; set; } = new();
+	public UserPhoto ProfilePicture { get; set; }
+	public UserPhoto BannerPicture { get; set; }
+	public List<UserReaction> UserReactions { get; set; } = new();
+
+	public ICollection<AppUserRole> UserRoles { get; set; }
+
+	public List<Message> MessagesSent { get; set; }
+	public List<Message> MessagesReceived { get; set; }
+
+	public List<Block> BlocksInitiated { get; set; } = new();
+	public List<Block> BlocksReceived { get; set; } = new();
+	public List<Notification> Notifications { get; set; } = new();
+}
