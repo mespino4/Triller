93865df3140d1fb5002f015fb43b11ebee40783a
Mely Espino <90160548+mespino4@users.noreply.github.com>
2023-12-23 01:05:10 -0600

    seed user data

--- api-aspnet/obj/Debug/net8.0/api-aspnet.AssemblyInfo.cs
+++ api-aspnet/obj/Debug/net8.0/api-aspnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-aspnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea1cd4a2b411ae360783821622adf8f97b0e80ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-aspnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-aspnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
