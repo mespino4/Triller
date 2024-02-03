60c62aa54595cb50f1dc1668475811f311109d0f
Mely Espino <90160548+mespino4@users.noreply.github.com>
2024-02-03 09:50:49 -0600

    update

--- api-aspnet/obj/Debug/net8.0/api-aspnet.AssemblyInfo.cs
+++ api-aspnet/obj/Debug/net8.0/api-aspnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-aspnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc8fe5c91ea96e992a0ce89ff1b423658e0553c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba97889f3488d1b7b12ca6d4c7018bd6c502a917")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-aspnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-aspnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
